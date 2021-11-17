aef2cf592cb741b80a562abe0a238e017f1cd4ad
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-17 15:46:09 -0500

    Repository wrapper

---
+++ Repository.js
@@ -0,0 +1,24 @@
+const { $git, $octokit, $repository } = require('./symbols')
+
+module.exports = class Repository {
+  async createBranch (name, from = 'main') {
+    const { data: fromRef } = await this[$octokit].request(`GET /repos/${this[$repository]}/git/refs/heads/${from}`)
+    const response = await this[$octokit].request(`POST /repos/${this[$repository]}/git/refs`, {
+      ref: `refs/heads/${name}`,
+      sha: fromRef.object.sha
+    })
+    if (response.status !== 201) {
+      throw new Error('Not able to create branch')
+    }
+  }
+
+  async clone (branchName = 'main') {
+    throw new Error('Not implemented')
+  }
+
+  constructor (git, octokit, repositoryName) {
+    this[$git] = git
+    this[$octokit] = octokit
+    this[$repository] = repositoryName
+  }
+}
