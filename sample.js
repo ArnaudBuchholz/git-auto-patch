4013188469c3a3ad8be368a1450d3e6c6048effc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-18 11:47:43 -0500

    Sample script

--- sample.js
+++ sample.js
@@ -1,6 +1,3 @@
-const { readFile, writeFile } = require('fs/promises')
-const { join } = require('path')
-
 module.exports = async git => {
   const projects = [
     'git-auto-patch'
@@ -9,9 +6,9 @@ module.exports = async git => {
     const repository = git.repository(`ArnaudBuchholz/${projectName}`)
     const branchName = `patch-${new Date().toISOString().replace(/:|T|\.|z/ig, '')}`
     await repository.createBranch(branchName, 'main')
-    const path = await repository.clone(branchName)
-    const sampleContent = (await readFile(join(path, 'sample.txt'))).toString()
-    await writeFile(join(path, 'sample.txt'), sampleContent + `\n${branchName}`)
+    await repository.checkout(branchName)
+    const sampleContent = await repository.readFile('sample.txt')
+    await repository.writeFile('sample.txt', sampleContent + `\n${branchName}`)
     await repository.commitAllAndPush('This is a sample message')
     await repository.createPullRequest(branchName, 'main')
   }
