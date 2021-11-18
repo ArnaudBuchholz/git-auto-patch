be294756d1f19989c023714f4e2868fb5bec4278
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-18 11:42:49 -0500

    Improves the logic and traces

--- git.js
+++ git.js
@@ -1,33 +1,22 @@
-const { execFile } = require('child_process')
-const { mkdir, stat, rm: newRm, rmdir } = require('fs/promises')
+const util = require('util')
+const execFile = util.promisify(require('child_process').execFile)
 
-const rm = newRm ?? rmdir
-const recursive = { recursive: true }
-
-async function cleanDir (dir) {
-  try {
-    await stat(dir)
-    await rm(dir, recursive)
-  } catch (err) {
-    // Ignore
-  }
-}
-
-function append (chunk) {
-  this.push(chunk.toString())
-}
-
-module.exports = async cwd => {
-  await cleanDir(cwd).then(() => mkdir(cwd, recursive))
-  return function git (...args) {
-    return new Promise((resolve, reject) => {
-      const cmd = execFile('git', args, { cwd })
-      const stdout = []
-      const stderr = []
-      cmd.stdout.on('data', append.bind(stdout))
-      cmd.stderr.on('data', append.bind(stderr))
-      cmd.on('error', reject)
-      cmd.on('close', () => resolve({ stdout: stdout.join(''), stderr: stderr.join('') }))
-    })
+module.exports = cwd => {
+  return async function git (...args) {
+    console.log(`${cwd}>git`, ...args.map(arg => {
+      const isUrl = arg.match(/^https:\/\/([^:]+):([^@]+)@/)
+      if (isUrl) {
+        return arg.replace(isUrl[0], `https://${isUrl[1]}:[REDACTED]@`)
+      }
+      return arg
+    }))
+    const { stdout, stderr } = await execFile('git', args, { cwd })
+    if (stdout) {
+      console.log(stdout)
+    }
+    if (stderr) {
+      console.log(stderr)
+    }
+    return { stdout, stderr }
   }
 }
